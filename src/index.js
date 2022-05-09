927cf1af1355da10eb54c7fbe8a9e2a13168a86f
sniedzielski <sniedzielski@soldevelo.com>
2022-05-09 11:26:14 +0200

    renamed template into language inmodule name

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
 }
 
-export const TemplateModule = (cfg) => {
+export const LanguageEnGmbModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
